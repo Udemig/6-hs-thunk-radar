9874fbd73fdd10f83edbb5ad52d375d1427b8e58
furkanevin <furkanevin00@gmail.com>
2023-09-10 02:12:52 +0300

    first commit

---
+++ src/pages/MapView.jsx
@@ -0,0 +1,53 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import { useSelector } from 'react-redux';
+
+const MapView = ({ openModal }) => {
+  const store = useSelector((store) => store);
+
+  return (
+    <div>
+      <MapContainer
+        center={[38.728587, 35.173275]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {/* her bir uçuş için ekrana marker basar */}
+        {store.flights.map((flight) => (
+          <Marker key={flight.id} position={[flight.lat, flight.lng]}>
+            <Popup>
+              <div className="popup">
+                <span>Kod: {flight.code}</span>
+                <button onClick={() => openModal(flight.id)}>
+                  Detay
+                </button>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+
+        {/* <Polyline
+          pathOptions={[
+            [51.505, -0.09],
+            [51.51, -0.1],
+            [51.51, -0.12],
+          ]}
+        /> */}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
